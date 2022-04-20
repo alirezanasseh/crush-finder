ea53f0bf6e392f0f84d3a9260136c3ff22e1e61d
Alireza <alireza.nasseh@gmail.com>
2022-04-20 19:03:50 +0430

    Send request by package

--- src/login/index.tsx
+++ src/login/index.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
-import axios from 'axios';
+import TwitterLogin from 'react-twitter-login';
+// import axios from 'axios';
 
 export default function Login() {
-    const step1 = () => {
-        axios({
-            url: 'https://api.twitter.com/oauth/request_token?oauth_callback=https://crush-finder.vercel.app',
-            method: 'post',
-            headers: {
-                'Authorization': `OAuth ` +
-                `oauth_callback="https://crush-finder.vercel.app",` +
-                `oauth_consumer_key="${process.env.REACT_APP_API_KEY}"`
-            }
-        })
+    // const step1 = () => {
+    //     axios({
+    //         url: 'https://api.twitter.com/oauth/request_token?oauth_callback=https://crush-finder.vercel.app',
+    //         method: 'post',
+    //         headers: {
+    //             'Authorization': `OAuth ` +
+    //             `oauth_callback="https://crush-finder.vercel.app",` +
+    //             `oauth_consumer_key="${process.env.REACT_APP_API_KEY}"`
+    //         }
+    //     })
+    // };
+
+    const authHandler = (err: any, data: any) => {
+        console.log(err, data);
     };
 
     return (
         <div>
             <h1>Login</h1>
-            <button onClick={step1}>Go</button>
+            <TwitterLogin
+                consumerKey={process.env.REACT_APP_API_KEY || ''}
+                consumerSecret={process.env.REACT_APP_API_KEY_SECRET || ''}
+                authCallback={authHandler}
+            />
+            {/*<button onClick={step1}>Go</button>*/}
         </div>
     );
 }

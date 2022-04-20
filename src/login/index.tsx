6aae57213dc30c1264c96212b5d481deddc8c050
Alireza <alireza.nasseh@gmail.com>
2022-04-20 12:58:12 +0430

    Send request manually

--- src/login/index.tsx
+++ src/login/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import TwitterLogin from 'react-twitter-login';
+import axios from 'axios';
 
 export default function Login() {
-    const authHandler = (err: Error, data: any) => {
-        console.log(err, data);
+    const step1 = () => {
+        axios({
+            url: 'https://api.twitter.com/oauth/request_token?oauth_callback=https://crush-finder.vercel.app',
+            method: 'post',
+        })
     };
 
     return (
         <div>
             <h1>Login</h1>
-            <TwitterLogin
-                consumerKey={process.env.REACT_APP_API_KEY || ''}
-                consumerSecret={process.env.REACT_APP_API_KEY_SECRET || ''}
-                authCallback={authHandler}
-            />
+            <button onClick={step1}>Go</button>
         </div>
     );
 }
